afb662623f881ed5603a220f11375da48c2ddebb
rsgas <mribrahimabim@gmail.com>
2025-01-18 00:39:04 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="col">
+          <h2 className='text-center'><i>O RSGA'S</i></h2>
+          <br/>
+          <p><span className="big">N</span>o final do século XV, a Europa vivia a Era das Grandes Navegações. Reinos como Espanha, Portugal, Inglaterra e França competiam para descobrir novas rotas comerciais, principalmente para acessar especiarias, ouro e outros recursos valiosos do Oriente. Nesse contexto, um navegante chamado Cristóvão Colombo desempenhou um papel crucial ao chegar a um novo continente: a América.</p>
+          <br />
+          <p>Cristóvão Colombo nasceu em Gênova, na Itália, e desde jovem mostrou interesse pelo mar e pela navegação. Ele acreditava firmemente que a Terra era redonda (uma ideia já aceita entre os estudiosos da época) e tinha um plano ousado: alcançar as Índias Orientais navegando para o oeste, atravessando o Oceano Atlântico.
+
+          Naquela época, a rota tradicional para as Índias era longa e perigosa, contornando a África. Colombo apresentou seu projeto a vários monarcas europeus, mas foi rejeitado por anos, até que conseguiu o apoio dos reis católicos da Espanha, Fernando e Isabel.</p>
+
+          <br />
+
+          <a href="https://www.youtube.com/watch?v=bSMMCJiC-kk">Quem Foi</a>
+
+          <br />
+
+          <p>Em 3 de agosto de 1492, Colombo partiu do porto de Palos de la Frontera com três embarcações: a Santa Maria, a Pinta e a Niña. Ele e sua tripulação enfrentaram semanas de incertezas, tempestades e o medo do desconhecido. Muitos acreditavam que cairiam no "fim do mundo", mas Colombo manteve sua tripulação motivada.
+
+          Após cerca de dois meses de viagem, na madrugada de 12 de outubro de 1492, um marinheiro chamado Rodrigo de Triana avistou terra. Eles haviam chegado a uma ilha nas Bahamas, que Colombo chamou de San Salvador. Embora acreditasse ter alcançado as Índias, na verdade, ele estava diante de um continente totalmente novo para os europeus.</p>
+
+          <img src="https://asset.kompas.com/crops/LLUYyH7tlqxg8eqIwzz-QF4bR9A%3D/101x0%3A776x450/1200x800/data/photo/2021/10/25/617613b3ebb05.jpg" alt="Fotos Das Ruas" style={
+            {
+              width: '200px',
+              margin: '20px 0'
+            }
+          } />
+
+          <br />
+
+          <p>Ao desembarcar, Colombo encontrou povos indígenas com culturas e línguas desconhecidas. Ele os chamou de "índios", acreditando que estava nas Índias. Os habitantes locais viviam em harmonia com a natureza e possuíam uma rica tradição oral. Colombo ficou impressionado com a beleza das terras e começou a explorar a região, documentando tudo o que via.
+
+          Ele também coletou ouro, animais exóticos e outros recursos para levar de volta à Espanha, convencendo os reis de que sua viagem havia sido um sucesso.</p>
+
+          <br />
+          
+          <a href="https://www.youtube.com/watch?v=MxhqXaW1A_s"> o descobridor da América</a>
+
+          <br />
+
+          <p>Colombo realizou mais três viagens ao Novo Mundo nos anos seguintes, explorando partes do Caribe, América Central e do Sul. No entanto, ele morreu em 1506, sem saber que havia descoberto um continente que mudaria o curso da história mundial.
+
+          Embora Colombo tenha sido celebrado como um grande explorador, sua chegada à América teve consequências devastadoras para os povos indígenas, incluindo a exploração, a violência e a introdução de doenças que dizimaram populações inteiras.</p>
+
+          <br />
+
+          <p>Hoje, Cristóvão Colombo é lembrado como uma figura controversa: um visionário que abriu caminho para o intercâmbio cultural e econômico entre os continentes, mas também como alguém cujo impacto marcou o início de séculos de colonização e exploração. Sua "descoberta" da América é considerada um dos eventos mais significativos da história mundial, iniciando a era moderna da globalização.</p>
+
+          <div className='wrp'>
+            <div className="hr"></div>
+          </div>
+
+          <div className="text-center">
+            <i className='fs-6'>© RSGA'S - ALL RIGHT RESERVED</i>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
